86c03821e8865b813a708973855620dad192f9bf
Aniruddha Gawali <aniruddhagawali05@gmail.com>
2024-06-08 21:34:40 +0530

    feat: Improve loading component for drive page
    The code changes modify the loading component in `loading.tsx` to use `min-h-screen` instead of `h-full` for better compatibility with different screen sizes. This ensures that the loading UI covers the entire screen height.
    
    This commit improves the loading component for the drive page.

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function Loading({}: Props) {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 h-full w-full mt-24">
+    <div className="flex flex-col items-center justify-center gap-10 min-h-screen w-full mt-24">
       <LoaderCircle
         size={64}
         className="animate-spin"
